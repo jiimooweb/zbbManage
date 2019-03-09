da80d983452bc3476c69be123cb9e49e3a428450
dongfang <1163129165@qq.com>
2019-03-09 20:05:01 +0800

    更新暂用打包路径

--- vue.config.js
+++ vue.config.js
@@ -12,8 +12,8 @@ const resolve = dir => {
 // 需要将它改为'/my-app/'
 // iview-admin线上演示打包路径： https://file.iviewui.com/admin-dist/
 const BASE_URL = process.env.NODE_ENV === 'production'
-  ? '/'
-  : '/'
+  ? '/zbbManage/'
+  : '/zbbManage/'
 
 module.exports = {
   // Project deployment base
